19f9cf5871de7b3eb0fbd92b9476e78025ad0d96
KST <kstost@gmail.com>
2020-02-23 11:30:34 +0900

    fixed

--- charm_dom.js
+++ charm_dom.js
@@ -3,34 +3,34 @@
 import React, { useState, useRef } from 'react'
 let cmm = require('./lib/ext');
 function charmDOM(ref) {
-    let t_rnode;// = this.rnode;
-    let uob = {
-        rdom() {
-            if (t_rnode && t_rnode.parentNode) {
-                if (t_rnode.custom_box && t_rnode.custom_box.Knimation) {
-                    t_rnode.custom_box.Knimation.remove_dts_from_dom(t_rnode);
-                }
-                t_rnode.parentNode.removeChild(t_rnode);
+    let t_rnode;
+    function rdom() {
+        if (t_rnode && t_rnode.parentNode) {
+            if (t_rnode.custom_box && t_rnode.custom_box.Knimation) {
+                t_rnode.custom_box.Knimation.remove_dts_from_dom(t_rnode);
             }
-            let refNode = ref.current;
-            if (refNode) {
-                t_rnode = refNode.cloneNode(true);
-                let listv = cmm.walk_recursive(refNode, []);
-                let listc = cmm.walk_recursive(t_rnode, []);
-                for (let i = 0; i < listc.length; i++) {
-                    cmm.cpp(listv[i], listc[i]);
-                }
-                cmm.insertAfter(t_rnode, refNode);
-                return t_rnode;
+            t_rnode.parentNode.removeChild(t_rnode);
+        }
+        let refNode = ref.current;
+        if (refNode) {
+            t_rnode = refNode.cloneNode(true);
+            let listv = cmm.walk_recursive(refNode, []);
+            let listc = cmm.walk_recursive(t_rnode, []);
+            for (let i = 0; i < listc.length; i++) {
+                cmm.cpp(listv[i], listc[i]);
             }
+            cmm.insertAfter(t_rnode, refNode);
+            return t_rnode;
+        }
+    };
+    return [
+        {
+            style: { display: 'none' },
+            ref: ref,
         },
-    }
-    return [{
-        style: { display: 'none' },
-        ref: ref,
-    }, uob.rdom];
+        rdom
+    ];
 }
 export default function () {
-    let dd = charmDOM(useRef());
-    return useRef(dd).current;
+    return useRef(charmDOM(useRef())).current;
 };
