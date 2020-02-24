35e2f261bd451a346183e5ab1f31ad75bce9c76c
KST <kstost@gmail.com>
2020-02-24 12:24:18 +0900

    fixed

--- charm_dom.js
+++ charm_dom.js
@@ -7,7 +7,10 @@ import very_global from 'very_global'
 let cmm = require('./lib/ext');
 function charmDOM(ref) {
     let t_rnode;
-    function rdom() {
+    function rdom(p_req) {
+        if (p_req) {
+            return t_rnode;
+        }
         if (t_rnode && t_rnode.parentNode) {
             if (t_rnode.custom_box && t_rnode.custom_box.Knimation) {
                 t_rnode.custom_box.Knimation.remove_dts_from_dom(t_rnode);
